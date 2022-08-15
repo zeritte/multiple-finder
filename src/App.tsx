123c400f5d2cb59cd51ff85b05c7590496d295e4
zeritte <zeritte@gmail.com>
2022-08-15 10:40:44 +0100

    use NumberButton component and implement multiples logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import NumberButton from "./components/NumberButton";
 
@@ -7,9 +8,30 @@ const NUMBERS_ARRAY = [...Array(MAX_NUMBER + 1).keys()].slice(1);
 function App() {
   const [selectedNumber, setSelectedNumber] = useState<number>();
   const [multiples, setMultiples] = useState<number[]>([]);
+
+  useEffect(() => {
+    if (!selectedNumber) return;
+    setMultiples(
+      NUMBERS_ARRAY.filter((n) => !(n % selectedNumber) && n > selectedNumber)
+    );
+  }, [selectedNumber]);
+
+  const onButtonClick = (num: number) => {
+    setSelectedNumber(num);
+  };
+
   return (
     <main className="main">
       <div className="container">
+        {NUMBERS_ARRAY.map((number) => (
+          <NumberButton
+            key={number}
+            number={number}
+            isSelected={number === selectedNumber}
+            isInMultiples={multiples.includes(number)}
+            onClick={onButtonClick}
+          />
+        ))}
       </div>
     </main>
   );
