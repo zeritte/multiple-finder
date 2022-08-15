16f1c2570007af84893132ad4e7e047de75140c4
zeritte <zeritte@gmail.com>
2022-08-15 10:27:27 +0100

    add in the number button component

---
+++ src/components/NumberButton.tsx
@@ -0,0 +1,25 @@
+type NumberButtonProps = {
+  number: number;
+  isSelected: boolean;
+  isInMultiples: boolean;
+  onClick: (value: number) => void;
+};
+
+export default function NumberButton({
+  number,
+  onClick,
+  isSelected,
+  isInMultiples,
+}: NumberButtonProps) {
+  return (
+    <button
+      onClick={() => onClick(number)}
+      className={`number-button
+                ${isSelected ? "highlight-selected" : ""}
+                ${isInMultiples ? "highlight-multiple" : ""}`}
+      key={number}
+    >
+      {number}
+    </button>
+  );
+}
