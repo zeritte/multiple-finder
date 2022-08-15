3cf676a24aa455d89cc72d0c31fbf31b28c2d17c
zeritte <zeritte@gmail.com>
2022-08-15 13:25:51 +0100

    lint

--- src/__tests__/NumberButton.test.tsx
+++ src/__tests__/NumberButton.test.tsx
@@ -4,39 +4,67 @@ import NumberButton from "../components/NumberButton";
 describe("Test NumberButton component", () => {
   it("SHOULD render the given number", () => {
     const number = 1;
-    render(<NumberButton number={number} isSelected={false} isInMultiples={false} onClick={() => {}} />);
+    render(
+      <NumberButton
+        number={number}
+        isSelected={false}
+        isInMultiples={false}
+        onClick={() => {}}
+      />
+    );
     const numberButton = screen.getByTestId(`select-number-${number}`);
     expect(numberButton).toBeInTheDocument();
-    expect(numberButton).toHaveTextContent(number.toString())
+    expect(numberButton).toHaveTextContent(number.toString());
   });
 
   it("SHOULD render the given number when selected", () => {
     const number = 123;
-    render(<NumberButton number={number} isSelected={true} isInMultiples={false} onClick={() => {}} />);
+    render(
+      <NumberButton
+        number={number}
+        isSelected={true}
+        isInMultiples={false}
+        onClick={() => {}}
+      />
+    );
     const numberButton = screen.getByTestId(`select-number-${number}`);
     expect(numberButton).toBeInTheDocument();
-    expect(numberButton).toHaveTextContent(number.toString())
-    expect(numberButton).toHaveClass('highlight-selected')
+    expect(numberButton).toHaveTextContent(number.toString());
+    expect(numberButton).toHaveClass("highlight-selected");
   });
 
   it("SHOULD render the given number when in multiples", () => {
     const number = 123;
-    render(<NumberButton number={number} isSelected={false} isInMultiples={true} onClick={() => {}} />);
+    render(
+      <NumberButton
+        number={number}
+        isSelected={false}
+        isInMultiples={true}
+        onClick={() => {}}
+      />
+    );
     const numberButton = screen.getByTestId(`select-number-${number}`);
     expect(numberButton).toBeInTheDocument();
-    expect(numberButton).toHaveTextContent(number.toString())
-    expect(numberButton).toHaveClass('highlight-multiple')
+    expect(numberButton).toHaveTextContent(number.toString());
+    expect(numberButton).toHaveClass("highlight-multiple");
   });
 
   it("SHOULD fire the onClick function on button press", () => {
     const number = 123;
     const mockOnClick = jest.fn();
-    render(<NumberButton number={number} isSelected={false} isInMultiples={false} onClick={mockOnClick} />);
+    render(
+      <NumberButton
+        number={number}
+        isSelected={false}
+        isInMultiples={false}
+        onClick={mockOnClick}
+      />
+    );
     const numberButton = screen.getByTestId(`select-number-${number}`);
     expect(numberButton).toBeInTheDocument();
-    expect(numberButton).toHaveTextContent(number.toString())
-    fireEvent.click(numberButton)
-    expect(mockOnClick).toHaveBeenCalledWith(number)
-    expect(mockOnClick).toHaveBeenCalledTimes(1)
+    expect(numberButton).toHaveTextContent(number.toString());
+    fireEvent.click(numberButton);
+    expect(mockOnClick).toHaveBeenCalledWith(number);
+    expect(mockOnClick).toHaveBeenCalledTimes(1);
   });
 });
